f314dbd670efe13290f05b989020981ca7c91b84
Lucas Carneiro <luscrn.dev@gmail.com>
2024-10-17 20:09:29 -0300

    darkmode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 ScrollReveal({ reset: true });
 
 ScrollReveal().reveal(".hidden", { duration: 2000, distance: "2rem" });
+
+const theme = document.querySelector("#theme");
+const body = document.querySelector("body");
+
+theme.addEventListener("click", (e) => {
+    theme.classList.toggle("fa-sun");
+    theme.classList.toggle("fa-moon");
+    body.classList.toggle("darkmode");
+});
